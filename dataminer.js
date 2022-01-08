9c8355f59bad153c3db961f3cbca43c52741b9d4
unknown <richsonsimbabure@gmail.com>
2022-01-08 05:32:17 -0500

    added a twitter data miner and an api call in react

---
+++ dataminer.js
@@ -0,0 +1,12 @@
+var a = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium'
+  ];
+
+  var a1 = a.map(s=>s.length)
+ var a2 = a.map(function(){
+     return(a.length)
+ })
+  console.log(a2)
